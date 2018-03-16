e4af4502acaeb01164b29a2386b920d3f0eebe70
Bowen <bowen.sui@hotmail.com>
2018-03-16 16:30:03 +0100

    some new files for seciton 3

---
+++ di-ioc/DmitriNesteruk/s03/m16_PropertyMethodInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmitriNesteruk.s03
+{
+    class m16_PropertyMethodInjection
+    {
+    }
+}
---
+++ di-ioc/DmitriNesteruk/s03/m17_ScanningForTypes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmitriNesteruk.s03
+{
+    class m17_ScanningForTypes
+    {
+    }
+}
